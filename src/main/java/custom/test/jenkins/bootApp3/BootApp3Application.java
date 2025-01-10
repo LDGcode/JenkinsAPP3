ae92f0082a7488eda3259fe6de1d068f1dc1d463
administrastor <example@gmail.com>
2025-01-10 13:32:45 +0800

    first commit

---
+++ src/main/java/custom/test/jenkins/bootApp3/BootApp3Application.java
@@ -0,0 +1,13 @@
+package custom.test.jenkins.bootApp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootApp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootApp3Application.class, args);
+	}
+
+}
---
+++ src/test/java/custom/test/jenkins/bootApp3/BootApp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package custom.test.jenkins.bootApp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
